3c3ef6530af7144e5da6906495c97ef8e32d8dd1
DaweiYin <davidyin@bu.edu>
2022-10-18 01:43:25 -0400

    update the method for calculating multiple choice scores

--- data_structure/main.py
+++ data_structure/main.py
@@ -10,7 +10,7 @@ illustrate the steps for creating survey and answer the survey from students
 """
 
 # step 1: you need a survey, so create one
-survey_1 = Survey(survey_name="hello", survey_id= "unique_id", questions=None, students=None)
+survey_1 = Survey(survey_name="hello", survey_id="unique_id", questions=None, students=None)
 
 # step 2: you need to set questions for the survey
 """
@@ -27,25 +27,18 @@ for example: if you have 3 questions:
 question_1 = SingleChoiceQuestion(question_name="q1",
                                   description="None",
                                   weight=1,
-                                  choices=["Java", "C", "Python","R"])
+                                  choices=["Java", "C", "Python", "R"])
 
 question_2 = SingleChoiceQuestion(question_name="q2",
                                   description="None",
                                   weight=-1,
-                                  choices=["Java", "C", "Python","C++"])
+                                  choices=["Java", "C", "Python", "C++"])
 
-question_3 = SingleChoiceQuestion(question_name="q2",
-                                  description="None",
-                                  weight=-1,
-                                  choices=["Java", "C", "Python","C++"])
-
-
-
-#question_3 = MultipleChoiceQuestion(question_name="q3",
-#                                    description="None",
-#                                    weight=1,
-#                                    choices=["Java", "C", "Python", "C++"],
-#                                    max_num_choice=3)
+question_3 = MultipleChoiceQuestion(question_name="q3",
+                                    description="None",
+                                    weight=1,
+                                    choices=["Java", "C", "Python", "C++", "R", "Ruby"],
+                                    max_num_choice=3)
 
 # step 3: add question to the survey
 survey_1.append_question(question_1)
@@ -54,10 +47,10 @@ survey_1.append_question(question_3)
 print(len(survey_1.questions))
 
 # step 4: need to have student to answer the survey, so manually set 4 student here
-student_1 = Student(name="stu 1", email="test@gmail.com",user_id= 1)
-student_2 = Student(name="stu 2", email="test@gmail.com", user_id= 2)
-student_3 = Student(name="stu 3", email="test@gmail.com", user_id= 3)
-student_4 = Student(name="stu 4", email="test@gmail.com", user_id= 4)
+student_1 = Student(name="stu 1", email="test@gmail.com", user_id=1)
+student_2 = Student(name="stu 2", email="test@gmail.com", user_id=2)
+student_3 = Student(name="stu 3", email="test@gmail.com", user_id=3)
+student_4 = Student(name="stu 4", email="test@gmail.com", user_id=4)
 
 # step 5: student respond to the survey
 
@@ -65,24 +58,24 @@ student_4 = Student(name="stu 4", email="test@gmail.com", user_id= 4)
 # (key = question in that survey, value = index of the option of the question)
 
 student_1_answer = {
-	0: 3,  # single choice
-	1: 2,  # multiple choice
-	2: {1: 2, 2: 3, 3: 4},  # multiple choice
+    0: 3,  # single choice
+    1: 2,  # multiple choice
+    2: {0: 2, 1: 3, 2: 1}  # multiple choice
 }
 student_2_answer = {
-	0: 3,  # single choice
-	1: 1,  # multiple choice
-	2: {1: 2, 2: 3, 3: 4},  # multiple choice
+    0: 3,  # single choice
+    1: 1,  # multiple choice
+    2: {0: 2, 1: 4, 2: 5}  # multiple choice
 }
 student_3_answer = {
-	0: 1,  # single choice
-	1: 1,  # multiple choice
-	2: {1: 2, 2: 3, 3: 4},  # multiple choice
+    0: 1,  # single choice
+    1: 1,  # multiple choice
+    2: {0: 2, 1: 3, 2: 4}  # multiple choice
 }
 student_4_answer = {
-	0: 1,  # single choice
-	1: 2,  # multiple choice
-	2: {1: 2, 2: 3, 3: 4},  # multiple choice
+    0: 1,  # single choice
+    1: 2,  # multiple choice
+    2: {0: 2, 1: 3}  # multiple choice
 }
 
 # step 6. student submit their response
@@ -94,11 +87,11 @@ student_4.answer_survey(survey_1, student_4_answer)
 # step 7. randomly assigned 4 student to the 2 teams, but here for the sake of illustration, I manually assign them
 team_1 = Team(team_size=4, team_name="t1", survey_target=survey_1)
 team_2 = Team(team_size=2, team_name="t2", survey_target=survey_1)
-team_1.team_members = [student_1, student_2,student_3,student_4]
+team_1.team_members = [student_1, student_2, student_3, student_4]
 
 # step 8. calculate the two team scores?
 
 ## please write score function for the team class
 print(f"team_1 scores for single choice are {team_1.get_single_choice_scores()}")
-
+team_1.get_mul_choices_scores()
 print()
--- data_structure/team.py
+++ data_structure/team.py
@@ -1,8 +1,8 @@
 class Team:
-	total_score = 0
+    total_score = 0
 
-	def __init__(self, team_size, team_name, survey_target, **kwargs):
-		"""
+    def __init__(self, team_size, team_name, survey_target, **kwargs):
+        """
 
         :param team_size: the size of the team
         :param team_name: the name of the team
@@ -10,79 +10,141 @@ class Team:
         :param survey: the survey that is targeting for calculating the team score
         :param kwargs: reserved
         """
-		self.team_size = team_size
-		self.team_name = team_name
-		self.__team_members = None  # list of student
-		self.survey_target = survey_target
+        self.team_size = team_size
+        self.team_name = team_name
+        self.__team_members = None  # list of student
+        self.survey_target = survey_target
 
-	@property
-	def team_members(self):
-		# print(f"show group{self.team_name} members:{self.__team_members}")
-		return self.__team_members
+    @property
+    def team_members(self):
+        # print(f"show group{self.team_name} members:{self.__team_members}")
+        return self.__team_members
 
-	@team_members.setter
-	def team_members(self, members):
-		print("set team members")
-		self.__team_members = members
+    @team_members.setter
+    def team_members(self, members):
+        print("set team members")
+        self.__team_members = members
 
-	@team_members.deleter
-	def team_members(self):
-		print("deleting all members in the team")
-		del self.__team_members
+    @team_members.deleter
+    def team_members(self):
+        print("deleting all members in the team")
+        del self.__team_members
 
-	def add_team_member(self, student):
-		self.team_members.append(student)
+    def add_team_member(self, student):
+        self.team_members.append(student)
 
-	def get_team_size(self):
-		return len(self.__team_members)
+    def get_team_size(self):
+        return len(self.__team_members)
 
-	def get_single_choice_scores(self):
-		"""
+    def get_single_choice_scores(self):
+        """
         calculate all the single choice scores for the team
         :return: the score of all single choice questions of all members in the team
         """
 
-		# for each student,
-		# get single choice answers in the dictionary format {student id : {question index, answer obj}}
-		single_choice_answers = {}
-		for student in self.__team_members:
-			# do the query, get the student response(answer sheet) to that survey
-			student_answer_sheet = student.get_answer_sheet_by_survey(survey=self.survey_target)
-			# get the all single choice questions from the student answer sheet, and put it in the
-			# single_choice_questions dict
-			single_choice_answers[student.id] = student_answer_sheet.get_all_answers_by_question_type(
-				question_type="single")
+        # for each student,
+        # get single choice answers in the dictionary format {student id : {question index, answer obj}}
+        single_choice_answers = {}
+        for student in self.__team_members:
+            # do the query, get the student response(answer sheet) to that survey
+            student_answer_sheet = student.get_answer_sheet_by_survey(survey=self.survey_target)
+            # get the all single choice questions from the student answer sheet, and put it in the
+            # single_choice_questions dict
+            single_choice_answers[student.id] = student_answer_sheet.get_all_answers_by_question_type(
+                question_type="single")
+        # get the single choice question in the dictionary format {question index : question obj}
+        single_choice_questions = self.survey_target.get_all_questions_by_type(question_type="single")
 
-		# get the single choice question in the dictionary format {question index : question obj}
-		single_choice_questions = self.survey_target.get_all_questions_by_type(question_type="single")
+        # get the single choice question weight in the dictionary format {question index : weight}
+        single_choice_questions_weight = self.survey_target.get_all_question_weight_by_type(question_type="single")
 
-		# get the single choice question in the dictionary format {question index : weight}
-		single_choice_questions_weight = self.survey_target.get_all_question_weight_by_type(question_type="single")
+        # initialize result dict = {question index, score}
+        scores = {question_index: 0 for question_index in single_choice_questions.keys()}
 
-		# initialize result dict = {question index, score}
-		scores = {question_index: 0 for question_index in single_choice_questions.keys()}
+        # the calculation for the score
+        # for each single choice question
+        for question_index, single_question_obj in single_choice_questions.items():
+            unique_choice = set()
+            # for each student answer this question
+            for student in self.__team_members:
+                # print(student.id, single_question)
+                student_answer = single_choice_answers[student.id][question_index]
+                student_choice = student_answer.get_choice_result()
+                unique_choice.add(student_choice)
+            # find the total len of unique choice
+            unique_len = len(unique_choice)
+            scores[question_index] += (unique_len / len(self.__team_members)) * single_choice_questions_weight[
+                question_index]
 
-		# the calculation for the score
-		for question_index, single_question in single_choice_questions.items():
-			unique_choice = set()
-			for student in self.__team_members:
-				#print(student.id, single_question)
-				student_answer = single_choice_answers[student.id][question_index]
-				student_choice = student_answer.get_choice_result()
-				unique_choice.add(student_choice)
-			# find the total len of unique choice
-			unique_len = len(unique_choice)
-			scores[question_index] += (unique_len / len(self.__team_members)) * single_choice_questions_weight[
-				question_index]
+        return scores
 
-		return scores
+    def get_mul_choices_scores(self):
+        """
+        calculate all multiple choice scores for this team
+        @return: all multiple choice question scores for this team.
+        """
 
-	def get_mul_choices_score(self):
-		"""
-		calculate all multiple choice scores for this team
-		@return: all multiple choice question scores for this team.
-		"""
-		pass
+        # for each student, get multiple choice answers in the
+        # dictionary format {student id : {question index, multiple choice answer obj}}
+        multiple_choice_answers = {}
+        for student in self.__team_members:
+            # do the query, get the student response(answer sheet) to that survey
+            student_answer_sheet = student.get_answer_sheet_by_survey(survey=self.survey_target)
+            # get the all multiple choice questions from the student answer sheet, and put it in the
+            # multiple_choice_answers dict
+            multiple_choice_answers[student.id] = student_answer_sheet.get_all_answers_by_question_type(
+                question_type="multiple")
 
-	def get_sch_score(self):
-		pass
+        # get the multiple choice question in the dictionary format {question index : question obj}
+        multiple_choice_questions = self.survey_target.get_all_questions_by_type(question_type="multiple")
+
+        # get the multiple choice question weight in the dictionary format {question index : weight}
+        multiple_choice_questions_weight = self.survey_target.get_all_question_weight_by_type(question_type="multiple")
+
+        # max valid choices will be the size of this team
+        max_valid_choices = len(self.__team_members)
+
+        # initialize result dict = {question index, score}
+        scores = {question_index: 0 for question_index in multiple_choice_questions.keys()}
+
+        # the calculation for the score
+        # for each multiple question
+        for question_index, multiple_question_obj in multiple_choice_questions.items():
+            # find max number of choice for each multiple question
+            max_choice_num = min(multiple_question_obj.max_num_choice, max_valid_choices)
+            # initialize 0 score dict to record each option scores
+
+            # [[0,0]] [score for i option, number of occurrence for ith]
+            option_scores = [[0 for _ in range(2)] for _ in range(multiple_question_obj.get_choice_size())]
+
+            # for each student calculate and add the option score to the option_scores
+            for student in self.__team_members:
+                # get the student multiple choice result
+                choice_result = multiple_choice_answers[student.id][question_index].get_choice_result()
+
+                # according to the order, add choices score to the option_scores
+                for order, choice_index in enumerate(choice_result):
+                    # check if number of choice exceed the max number of choice
+                    if order > max_choice_num - 1:
+                        break
+                    # set choice weight, the larger order the less weight, choice index 0 has weight max_choice_num
+                    choice_weight = max_choice_num - order
+                    # update option_scores score and occurrence
+                    option_scores[choice_index][0] += choice_weight
+
+                    option_scores[choice_index][1] += 1
+
+            # after having option_scores, we can calculate the total scores for this question
+
+            # drop the scores with occurrence less or equal to 1
+            option_scores = [score for score in option_scores if score[1] > 1]
+            # calculate the total scores
+
+
+
+
+            pass
+        return multiple_choice_questions, multiple_choice_questions_weight, multiple_choice_answers
+
+    def get_sch_score(self):
+        pass
