5c8c3776160a665d2bb1089b97d4561ef3ccc087
WHaiyang11 <whaiyang@bu.edu>
2022-10-20 22:58:15 -0400

    Update ourtests.py

--- tests/ourtests.py
+++ tests/ourtests.py
@@ -1,15 +1,20 @@
 import unittest
-from data_structure import team
-from data_structure import question
-from data_structure import survey
-from data_structure import answer_sheet as an
+from code.app import team
+from code.app import question
+from code.app import survey
+from code.app import answer_sheet as an
+from code.app import instance
+from code.app import answer
+from code.app import user
+from code.app import run
+from code.app import main
 
 
 # test survey, not finished
 class question_test(unittest.TestCase):
     # set up test objects
     def setUp(self):
-        self.question1 = question.Question("q1", "test q1", "SingleChoiceQuestion", 1)
+        self.question1 = question.Question("q1", "test q1", 1)
 
     def test_question_init(self):
         # test question_name
@@ -17,9 +22,6 @@ class question_test(unittest.TestCase):
 
         # test description
         self.assertEqual(self.question1.description, "test q1")
-
-        # test question_type
-        self.assertEqual(self.question1.question_type, "SingleChoiceQuestion")
 
         # test weight
         self.assertEqual(self.question1.weight, 1)
@@ -32,17 +34,19 @@ class question_test(unittest.TestCase):
 class survey_test(unittest.TestCase):
     # set up test objects
     def setUp(self):
-        self.question1 = question.Question("q1", "test q1", "SingleChoiceQuestion", 1)
+        self.question1 = question.Question("q1", "test q1", 1)
+        self.question2 = question.Question("q2", "test q2", 1)
         self.questions1 = [self.question1]
         self.students1 = ["a", "b", "c"]
-        self.survey1 = survey.Survey("s1", self.questions1, self.students1)
+        self.survey1 = survey.Survey("survey 1", "1", self.questions1, self.students1)
 
+    # test initial function in survey
     def test_survey_init(self):
         # test survey_name
-        self.assertEqual(self.survey1.survey_name, "s1")
+        self.assertEqual(self.survey1.survey_name, "survey 1")
 
         # test questions
-        self.assertEqual(self.survey1.questions, self.questions1)
+        self.assertEqual(self.survey1.questions[0], self.question1)
 
         # test students
         self.assertEqual(self.survey1.students, self.students1)
@@ -50,9 +54,34 @@ class survey_test(unittest.TestCase):
         # test type
         self.assertTrue(isinstance(self.survey1, survey.Survey))
 
+    # test append_question in survey
+    def test_append_question(self):
+        # add a question
+        self.survey1.append_question(self.question2)
+
+        # check num_question
+        self.assertEqual(self.survey1.num_question, 2)
+
+        # check questions
+        self.assertEqual(self.survey1.questions[1], self.question2)
+
+    # test append_insert_question in survey, not finished
+    def test_insert_question(self):
+        self.assertEqual(1, 1)
+
+    # test get_all_question_indexes_by_type in survey, not finished
+    def get_all_question_indexes_by_type(self):
+        self.assertEqual(1, 1)
+
 
 # test answer, not finished
 class answer_test(unittest.TestCase):
+    def test(self):
+        self.assertEqual(1, 1)
+
+
+# test answer_sheet, not finished
+class answer_sheet_test(unittest.TestCase):
     def test(self):
         self.assertEqual(1, 1)
 
@@ -62,10 +91,10 @@ class team_test(unittest.TestCase):
 
     # set up test objects
     def setUp(self):
-        self.question1 = question.Question("q1", "test q1", "SingleChoiceQuestion", 1)
+        self.question1 = question.Question("q1", "test q1", 1)
         self.questions1 = [self.question1]
         self.students1 = ["a", "b", "c"]
-        self.survey1 = survey.Survey("s1", self.questions1, self.students1)
+        self.survey1 = survey.Survey("s1", "1", self.questions1, self.students1)
         self.team1 = team.Team(3, "good", self.survey1)
 
     # test initial function in team
