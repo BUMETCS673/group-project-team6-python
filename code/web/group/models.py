f1e4a994ccb12bfa381004fb9c921bce8a0b9e8c
Ruotong(Alex) Wang <alexwangf4000@gmail.com>
2022-10-27 08:18:53 +0800

    upload models

--- code/web/group/models.py
+++ code/web/group/models.py
@@ -1,3 +1,23 @@
 from django.db import models
 
 # Create your models here.
+
+# Group
+
+class Group(models.Model):
+    """
+    Group Model
+    """
+
+    group_id = models.AutoField(primary_key = True)
+    group_name = models.CharField(max_lengh = 50)
+    group_score = modles.IntegerField(max_length= 50)
+    survey = models.ForeignKey('Survey', on_delete = models)
+
+class Member(models.Model):
+    """
+    Member Model // Inheritance from student
+    """
+    student = modles.ForeignKey('Student', on_delete = models)
+
+
