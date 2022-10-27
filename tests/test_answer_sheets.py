a5c8c0537ea2ec3c638fd13f144718705d979f02
WHaiyang11 <whaiyang@bu.edu>
2022-10-26 23:31:29 -0400

    Create test_answer_sheets.py

---
+++ tests/test_answer_sheets.py
@@ -0,0 +1,14 @@
+import unittest
+from code.app import question
+from code.app import survey
+from code.app import answer_sheet
+
+
+# not finished
+class survey_test(unittest.TestCase):
+    def test(self):
+        self.assertEqual(1, 1)
+
+
+if __name__ == '__main__':
+    unittest.main()
