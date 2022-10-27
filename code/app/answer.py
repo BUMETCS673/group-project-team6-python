03c03677234ac4140010d7ec78e1e01acb1b227a
WHaiyang11 <whaiyang@bu.edu>
2022-10-27 19:43:15 -0400

    Update answer.py

--- code/app/answer.py
+++ code/app/answer.py
@@ -16,18 +16,23 @@ class Answer(ABC):
         @param survey: the question that in the survey
         @param kwargs: reserved
         """
-        self.question = question
-        self.question_type = question.question_type
-        self.survey = survey
+        # check None input
+        if question is not None:
+            self.question = question
+            self.question_type = question.question_type
+        else:
+            self.question = None
+            self.question_type = None
+        if survey is not None:
+            self.survey = survey
+        else:
+            self.survey = None
 
 
 class SingleChoiceAnswer(Answer):
     """
     the single choice answer class
     """
-    question_types = {
-        "single"
-    }
     answer_type = "single"
 
     def __init__(self, choice_result, **kwargs):
@@ -38,7 +43,12 @@ class SingleChoiceAnswer(Answer):
         @return: None
         """
         super().__init__(**kwargs)
-        self.choice_result = choice_result
+        # check if choice_result is None
+        if choice_result is not None:
+            self.choice_result = choice_result
+        else:
+            self.choice_result = None
+            print("choice_result is None")
 
     def get_choice_result(self):
         """
@@ -52,22 +62,20 @@ class MultipleChoiceAnswer(Answer):
     """
     the multiple choice answer class
     """
-    question_types = {
-        "multiple"
-    }
     answer_type = "multiple"
 
     def __init__(self, choices_result, **kwargs):
         """
 
-        @param choices_result: all choices result for the question, should be the dictionary format,
+        @param choices_result: all choices result for the question, should be the list,
         i.e. {0: 2, 1: 3, 2: 5} which {order of choice, the index of the option}
         @param weights_result: the weights for the choice
         @param kwargs:
         @return:
         """
         super().__init__(**kwargs)
-        self.choices_result = choices_result  # should be list, order is the index
+        # check if choice_result is None
+        self.choices_result = choices_result  # should be dict, dict key is the index of question
 
         # reserved for future
         # self.weights_result = weights_result
