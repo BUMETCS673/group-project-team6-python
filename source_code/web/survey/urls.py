77e9af47b3ca478b1747cbf413d67def7004049a
BaymaxShawn <95258216+BaymaxShawn@users.noreply.github.com>
2022-11-05 00:24:48 -0400

    add delete button

--- source_code/web/survey/urls.py
+++ source_code/web/survey/urls.py
@@ -8,6 +8,10 @@ app_name = "survey"
 urlpatterns = [
 	path('<slug:instance_slug>/create-update-survey/', views.create_update_survey, name='survey_create_update'),
 	path('survey/question/<int:question_id>/create_update_option/', views.create_update_options, name='options_create_update'),
+
+	#from shawn
+	path('survey/question/delete_options<int:question_id>',views.delete_options,name='delete_options'),
+
 	path('<slug:instance_slug>/survey-review/', views.review, name='review'),
 	path('<slug:instance_slug>/survey-lock/', views.survey_lock, name='lock'),
 	path('<slug:instance_slug>/survey-response-link/', views.survey_response, name='response')
