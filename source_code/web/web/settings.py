c324ee1deeaed0c64b1aed3a79f79ceb8a9df875
jamesyaooooo <56179490+jamesyaooooo@users.noreply.github.com>
2022-11-22 00:48:29 -0500

    Update settings.py

--- source_code/web/web/settings.py
+++ source_code/web/web/settings.py
@@ -11,6 +11,7 @@ https://docs.djangoproject.com/en/4.1/ref/settings/
 """
 
 from pathlib import Path
+import os
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -117,6 +118,7 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/4.1/howto/static-files/
 
 STATIC_URL = 'static/'
+STATICFILES_DIRS = [os.path.join(BASE_DIR, 'static')]
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.1/ref/settings/#default-auto-field
@@ -125,3 +127,5 @@ DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 
 LOGIN_REDIRECT_URL = "iGroup:home"  # the page after login, should set to home page
 LOGOUT_REDIRECT_URL = "welcome"
+
+
